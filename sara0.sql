28c959f055016bf0794cf8492f1ced2c1cd0124e
MiguelMR <miguelmontoyar@gmail.com>
2016-01-18 08:36:02 -0500

    V 3.02

--- sara0.sql
+++ sara0.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-01-2016 a las 13:36:52
+-- Tiempo de generación: 18-01-2016 a las 12:25:37
 -- Versión del servidor: 5.6.15-log
 -- Versión de PHP: 5.5.8
 
@@ -60,13 +60,13 @@ CREATE TABLE IF NOT EXISTS `aulas` (
 --
 
 INSERT INTO `aulas` (`id`, `estado`, `numero`, `capacidad`, `equipos`) VALUES
-(1, 1, 101, 0, 4),
-(2, 1, 102, 0, 3),
+(1, 1, 101, 0, 0),
+(2, 1, 102, 0, 0),
 (3, 1, 103, 0, 0),
 (4, 1, 104, 0, 0),
 (5, 1, 105, 0, 0),
 (6, 1, 106, 0, 0),
-(7, 1, 107, 0, 2),
+(7, 1, 107, 0, 0),
 (8, 1, 108, 0, 0),
 (9, 1, 109, 0, 0),
 (10, 1, 110, 0, 0),
@@ -90,7 +90,7 @@ INSERT INTO `aulas` (`id`, `estado`, `numero`, `capacidad`, `equipos`) VALUES
 (28, 1, 210, 0, 0),
 (29, 1, 211, 0, 0),
 (30, 1, 212, 0, 0),
-(31, 1, 213, 0, 1),
+(31, 1, 213, 0, 0),
 (32, 1, 214, 0, 0),
 (33, 1, 215, 0, 0),
 (34, 1, 216, 0, 0),
@@ -110,23 +110,7 @@ CREATE TABLE IF NOT EXISTS `equipos` (
   `aula` int(11) NOT NULL,
   `codigo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=11 ;
-
---
--- Volcado de datos para la tabla `equipos`
---
-
-INSERT INTO `equipos` (`id`, `estado`, `descrip`, `aula`, `codigo`) VALUES
-(1, 'Funciona', 'Un PC', 101, 'PC-001'),
-(2, 'Funciona', 'Un PC', 101, 'PC-002'),
-(3, 'Funciona', 'Un PC', 101, 'PC-003'),
-(4, 'Funciona', 'Un PC', 101, 'PC-004'),
-(5, 'Funciona', 'Un PC', 102, 'PC-005'),
-(6, 'Malo', 'Un PC', 102, 'PC-006'),
-(7, 'Funciona', 'Un PC', 102, 'PC-007'),
-(8, 'Funciona', 'Un PC', 207, 'PC-325'),
-(9, 'Funciona', 'Un PC', 207, 'PC-326'),
-(10, 'Poseido', 'Un PC', 213, 'PC-666');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -142,29 +126,7 @@ CREATE TABLE IF NOT EXISTS `eventos` (
   `aula` int(11) NOT NULL,
   `usuario` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=17 ;
-
---
--- Volcado de datos para la tabla `eventos`
---
-
-INSERT INTO `eventos` (`id`, `tipo`, `hora`, `fecha`, `aula`, `usuario`) VALUES
-(1, 1, '06:40:04', '2016-01-18', 102, 111111111),
-(2, 6, '06:40:27', '2016-01-18', 102, 0),
-(3, 0, '06:40:04', '2016-02-18', 102, 444444444),
-(4, 0, '06:40:04', '2016-02-20', 102, 444444444),
-(5, 0, '06:40:04', '2016-02-25', 102, 444444444),
-(6, 1, '06:40:04', '2016-03-25', 102, 222222222),
-(7, 1, '06:40:04', '2016-03-26', 102, 222222222),
-(8, 1, '06:40:04', '2016-04-07', 102, 333333333),
-(9, 1, '06:40:04', '2016-04-11', 202, 333333333),
-(10, 1, '06:40:04', '2016-03-11', 202, 333333333),
-(11, 1, '06:40:04', '2016-02-11', 202, 333333333),
-(12, 0, '06:40:04', '2016-01-11', 202, 555555555),
-(13, 2, '07:31:16', '2016-01-18', 202, 222222222),
-(14, 2, '07:31:35', '2016-01-18', 111, 222222222),
-(15, 0, '07:31:35', '2016-01-18', 111, 444444444),
-(16, 4, '07:35:17', '2016-01-18', 111, 222222222);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -181,17 +143,7 @@ CREATE TABLE IF NOT EXISTS `reservas` (
   `aula` int(11) NOT NULL,
   `doc` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=5 ;
-
---
--- Volcado de datos para la tabla `reservas`
---
-
-INSERT INTO `reservas` (`id`, `hora`, `fecha`, `docente`, `curso`, `aula`, `doc`) VALUES
-(1, 6, '2016-01-18', 'Uno Profe Sor', 'mate', 102, 111111111),
-(2, 8, '2016-01-18', 'Uno Profe Sor', 'mate', 112, 111111111),
-(3, 8, '2016-01-18', 'Dos Profe Sora', 'lecto', 102, 222222222),
-(4, 8, '2016-01-18', 'Tres Maes Tro', 'calculo', 213, 333333333);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -206,18 +158,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `apellidos` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `doc` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=6 ;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `tipo`, `nombre`, `apellidos`, `doc`) VALUES
-(1, 1, 'Uno', 'Profe Sor', 111111111),
-(2, 1, 'Dos', 'Profe Sora', 222222222),
-(3, 1, 'Tres', 'Maes Tro', 333333333),
-(4, 2, 'Cuatro', 'Moni Tor', 444444444),
-(5, 2, 'Cinco', 'Moni Tora', 555555555);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
